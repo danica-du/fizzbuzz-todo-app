75ea42525379635e11952afaed688eb8d3418177
danica-du <ddu@hmc.edu>
2022-04-01 02:05:44 -0700

    File formatting

--- src/Task.js
+++ src/Task.js
@@ -1,20 +1,20 @@
 import './styles/Task.css';
 
 function Task(props) {
-    return (
-        <div className='task-container'
-             onClick={() => props.onToggleTaskComplete(props.id)}
-        >
-            <span style={{ textDecoration: props.isCompleted ? "line-through" : "" }}
-            >
-                {props.content}
-            </span>
-            <button className='delete-btn'
-                    onClick={() => props.onTaskDelete(props.id)}>
-                X
-            </button>
-        </div>
-    );
+  return (
+    <div className='task-container'
+      onClick={() => props.onToggleTaskComplete(props.id)}
+    >
+      <span style={{ textDecoration: props.isCompleted ? "line-through" : "" }}
+      >
+        {props.content}
+      </span>
+      <button className='delete-btn'
+              onClick={() => props.onTaskDelete(props.id)}>
+        X
+      </button>
+    </div>
+  );
 }
 
 export default Task;
